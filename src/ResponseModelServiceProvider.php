1b6ad432e1c1e5cf2c6d55dc90d409a56ff5a0fc
Alex Goodwin <cyberia.resurrection@gmail.com>
2022-08-26 17:29:54 +1000

    Repoint provider

--- src/ResponseModelServiceProvider.php
+++ src/ResponseModelServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Cruxinator\ResponseModel;
 
-use Cruxinator\LaravelPackage\Package;
-use Cruxinator\LaravelPackage\PackageServiceProvider;
+use Cruxinator\Package\Package;
+use Cruxinator\Package\PackageServiceProvider;
 use Cruxinator\ResponseModel\Commands\ResponseModelCommand;
 
 class ResponseModelServiceProvider extends PackageServiceProvider
